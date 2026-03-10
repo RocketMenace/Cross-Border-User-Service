2d67b3374663f5960a8a3f09f1871034b1a6b45f
RocketMenace <grizzlygit777@gmail.com>
2026-03-10 17:22:21 +0300

    feat(domain)
    Implement password validation exception.

--- src/app/domain/exceptions/__init__.py
+++ src/app/domain/exceptions/__init__.py
@@ -3,6 +3,7 @@ from app.domain.exceptions.first_name import InvalidFirstNameError
 from app.domain.exceptions.last_name import InvalidLastNameError
 from app.domain.exceptions.middle_name import InvalidMiddleNameNameError
 from app.domain.exceptions.phone import InvalidPhoneError
+from app.domain.exceptions.raw_password import InvalidPasswordFormatError
 
 __all__ = [
     "InvalidEmailError",
@@ -10,4 +11,5 @@ __all__ = [
     "InvalidMiddleNameNameError",
     "InvalidPhoneError",
     "InvalidFirstNameError",
+    "InvalidPasswordFormatError",
 ]
---
+++ src/app/domain/exceptions/raw_password.py
@@ -0,0 +1,7 @@
+from app.domain.exceptions.base import DomainError
+
+
+class InvalidPasswordFormatError(DomainError):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
